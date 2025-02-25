3c1f76eb72057a791a54e6e5245c234a4089a377
danilbaybakov72 <danil.baybakov.72@gmail.com>
2025-02-25 15:40:50 +0500

    Update all

--- server/migrations/versions/25.02.2025_init_db.py
+++ server/migrations/versions/25.02.2025_init_db.py
@@ -1,8 +1,8 @@
-"""init db
+"""init_db
 
-Revision ID: 3683af61c4e1
+Revision ID: e8f519eeb716
 Revises:
-Create Date: 2025-02-25 10:20:34.877146
+Create Date: 2025-02-25 15:30:48.768458
 
 """
 
@@ -13,7 +13,7 @@ from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = "3683af61c4e1"
+revision: str = "e8f519eeb716"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
